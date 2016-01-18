380ff8d49f30d7ec5a5f6480337c41567fcbaf52
Karolina Wyszyńska <wyszynskak@student.mini.pw.edu.pl>
2016-01-19 00:12:49 +0100

    #poprawa aplikacji

--- biomarkery_slonce/shiny/server.R
+++ biomarkery_slonce/shiny/server.R
@@ -1,43 +1,105 @@
 library(shiny)
 library(ggplot2)
 library(survMisc)
+#All cancer types that we consider
 cancer_types<-c("BRCA","COAD","COADREAD","GBMLGG","KIPAN","KIRC","KIRP",
                 "LGG","LUAD","LUSC","OV","READ","UCEC")
+# cancer_types<-c("READ","UCEC")
 
+#Read in data and pvalues for each combination (cancer,gene)
 for(i in 1:length(cancer_types)){
-   eval(parse(text=paste0(cancer_types[i],"geny <- readRDS('../",cancer_types[i],"/geny.RDS')")))
-   eval(parse(text=paste0(cancer_types[i],"histy <- readRDS('../",cancer_types[i],"/histy.RDS')")))
-   eval(parse(text=paste0(cancer_types[i],"survy <- readRDS('../",cancer_types[i],"/survy.RDS')")))
+   eval(parse(text=paste0(cancer_types[i],"data <- readRDS('./data/",
+                          cancer_types[i],"dane.RDS')")))
+#    eval(parse(text=paste0(cancer_types[i],"pvalue <- readRDS('./data/",
+#                           cancer_types[i],"pvalue.RDS')")))
+   eval(parse(text=paste0(cancer_types[i],"genes_list <- readRDS('./data/",
+                          cancer_types[i],"genes_list.RDS')")))
 }
 
 shinyServer(function(input, output){
-   output$istotne_geny<-renderUI({
-      
-      eval(parse(text=paste0("geny <- ", input$rak, "geny")))
-      text <- paste('"', geny, '"', sep="")
-      text <- paste(text, text, sep="=", collapse=", ")
-      text <- paste0('list(', text, ')')
-      lista <- eval(parse(text=text))
-      selectInput("wybrany_gen", label=h3(strong("Istotne geny")), 
-                  choices=lista, selected=lista[[1]])
+   #Creating select list with genes
+   output$significant_genes<-renderUI({
+      #Creating list to show as : "GENE_NAME (p-value: VALUE)"
+      #Genes are already sorted from the most significat at the begining
+      #If the value wasn't counted pvalue is "-"
+      genes_list <- eval(parse(text=paste0("tmp <- ",input$cancer, 
+                                           "genes_list")))
+      selectInput("choosen_gene", label=h3(strong("Genes")), 
+                  choices=tmp, selected=tmp[[1]])
       
    })
+   #Creating stratum value slider for survival curve plot
+   output$plot_help_surv1 <- renderUI({
+     eval(parse(text=paste0("gene <- ",input$cancer, 
+                            "data[, '",input$choosen_gene,"']")))
+      max <- max(gene, na.rm=TRUE)
+      min <- min(gene, na.rm=TRUE)
+      med <- median(gene, na.rm=TRUE)
+      sliderInput("stratum_value_choosen", label="Stratum value: ", min=min,
+                    max=max,value=med, step=0.01, round=-2)
+
+   })
+   
+   #Creating text information under surv - plot
+   output$plot_help_surv2 <- renderText({
+     eval(parse(text=paste0("gene <- ",input$cancer, 
+                            "data[, '",input$choosen_gene,"']")))
+     n_low <- length(gene[gene<=input$stratum_value_choosen])
+     n_high <- length(gene[gene>input$stratum_value_choosen])
+      
+     paste0("Number of observations <=", input$stratum_value_choosen,
+                     ": ", n_low, br() , 
+                     "Number of observations >", input$stratum_value_choosen,
+                     ": ", n_high, br(),br(),
+                     strong("Attention:"), br(),
+                     "If the number of observations is small in one of 
+                      above groups
+                     the mRNA can seem to be significant, 
+                     but it does not have to be.",br(),br())
+     
+   })
+   
+   #Create text information under histogram plot
+   output$plot_help_hist <- renderUI({
+     eval(parse(text=paste0("gene <- ",input$cancer, 
+                            "data[, '",input$choosen_gene,"']")))
+     helpText(paste0("Number of observations: ",length(gene[!is.na(gene)])))
+     
+   })
+   
+   #Creating plot itself
    output$wykres <- renderPlot({
       if(input$typ=="Histogram"){
-         eval(parse(text=paste0("q <- ", input$rak, "histy[['",input$wybrany_gen,"']]")))
+        eval(parse(text=paste0("q <- ",
+                               input$cancer, "data[,'",input$choosen_gene,
+                               "',drop=FALSE]")))
          q<-ggplot(q,aes_string(names(q)[1]))+ geom_histogram(binwidth = 0.1)+
-            scale_y_continuous(name="Ilość oberwacji")+
-            scale_x_continuous(name="Wartości")+
-            ggtitle(paste0("Histogram mRNA ",input$wybrany_gen))+
+            scale_y_continuous(name="Number of observations")+
+            scale_x_continuous(name="Values")+
+            ggtitle(paste0("mRNA histogram ",input$wybrany_gen))+
             theme(plot.title = element_text(lineheight=20, face="bold"))
       } else {
-         eval(parse(text=paste0("q <- ", input$rak, "survy[['",input$wybrany_gen,"']]")))
-         eval(parse(text=paste0("l<-'krzywa przeżycia_dla_",input$wybrany_gen,"'")))
-         q<-autoplot(q,xLab="czas",yLab="1-P(wystąpienie zdarzenia)",
-                     title=paste0("Krzywa przeżycia dla mRNA ",input$wybrany_gen),
-                     legTitle = "Warstwa",
-                     legLabs=c("chorzy o wart. > mediana", "chorzy o wart. < mediana"))
+         eval(parse(text=paste0("tmp <- which(colnames(",input$cancer,
+                                "data)=='", input$choosen_gene,"')")))
+         eval(parse(text=paste0("dane <- ",input$cancer, "data[,c(1,",
+                                tmp,",(ncol(",
+                                input$cancer,"data)-2):ncol(",
+                                input$cancer,"data))]")))
+         dane$time <- dane$time/365
+         med <- input$stratum_value_choosen
+         eval(parse(text=paste0(
+              'q<-survfit(Surv(time, status == "dead")~(', input$choosen_gene, 
+                ">med), data=dane)")))
+         q<-autoplot(q,xLab="years",yLab="",
+                     title=paste0("Survival plot for mRNA ",input$choosen_gene,
+                                  "\n1-P(death)"),
+                     legTitle = "Stratum",
+                     legLabs=c(paste0("Patients with value < ",round(med,4)),
+                               paste0("Patients with value > ",
+                                      round(med,4))))
       }
       q
    })
 })
+
+
--- biomarkery_slonce/shiny/ui.R
+++ biomarkery_slonce/shiny/ui.R
@@ -4,37 +4,67 @@ library(survMisc)
 
 cancer_types<-c("BRCA","COAD","COADREAD","GBMLGG","KIPAN","KIRC","KIRP","LGG",
                 "LUAD","LUSC","OV","READ","UCEC")
-# Define UI for application that draws a histogram
+cancer_names <- c("Breast invasive carcinoma", "Colon adenocarcinoma",
+                  "Colon adenocarcinoma & Rectum adenocarcinoma",
+                  "Glioblastoma multiforme & Brain Lower Grade Glioma",
+                  "Kidney: Chromophobe, renal clear cell carcinoma, renal papillary cell carcinoma",
+                  "Kidney renal clear cell carcinoma",
+                  "Kidney renal papillary cell carcinoma","Brain Lower Grade Glioma",
+                  "Lung adenocarcinoma","Lung squamous cell carcinoma",
+                  "Ovarian serous cystadenocarcinoma","Rectum adenocarcinoma",
+                  "Uterine Corpus Endometrial Carcinoma")
+# cancer_types<-c("READ","UCEC")
+# cancer_names <- c("Rectum adenocarcinoma",
+#                   "Uterine Corpus Endometrial Carcinoma")
+
+
+cancer_list <- eval(parse(text=paste0("list(",paste('"',cancer_names,'"','=',
+                                                    '"',cancer_types,'"',
+                                              sep="", collapse=", "),")")))
+
+
 shinyUI(fluidPage(
-   img(src="t.png"),
+   img(src="english_title.png"),
    # Application title
-   headerPanel(title="", windowTitle="Przglądarka mRNA"),
-   #titlePanel("Aplikacja biomarkerów"),
-   HTML('<font size="1">Autorzy: Karolina Wyszyńska, Krzysztof Rudaś</font>'),
-   br(),
-   br(),
+   headerPanel(title="", windowTitle="mRNA Browser"),
    
+   tabsetPanel(tabPanel("Instructions", 
+                        helpText("hello world!"),
+                        br(),
+                        HTML('<font size="1">Version: 3.0 (18 January 2016)</font>'),
+                        br(),
+                        HTML('<font size="1">Authors: Karolina Wyszyńska, Krzysztof Rudaś</font>'),
+                        br()),
+   tabPanel("Application",
    # Sidebar with a slider input for the number of bins
    sidebarLayout(
       sidebarPanel(
-         selectInput("rak",
-                     label  = h3(strong("Typ raka")),
-                     choices  = cancer_types,
-                     selected = cancer_types[1]
+         selectInput("cancer",
+                     label  = h3(strong("Cancer type")),
+                     choices  = cancer_list,
+                     selected = cancer_list[1]
          ), 
-         uiOutput("istotne_geny"),
+         uiOutput("significant_genes"),
 
          radioButtons("typ",
-                      label  = h3(strong("Typ wykresu")),
+                      label  = h3(strong("Plot type")),
                       choices  = c("Histogram", 
-                                      "Krzywa przeżycia"),
+                                      "Survival curve"),
                       selected = "Histogram"
-         )
+         ),
+         conditionalPanel(condition="input.typ!='Histogram'",
+                          uiOutput("plot_help_surv1"), br(),br())
       ),
       
       # Show a plot of the generated distribution
       mainPanel(
-         plotOutput("wykres")
-      )
+         plotOutput("wykres"),
+         conditionalPanel(condition="input.typ=='Histogram'",
+         uiOutput("plot_help_hist")),
+         conditionalPanel(condition="input.typ!='Histogram'", br(),
+         uiOutput("plot_help_surv2"),br(),br(),br())
+      )),
+   id="choice", selected="description"
+   )
    )
 ))
